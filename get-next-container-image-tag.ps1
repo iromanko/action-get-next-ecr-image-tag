58772ae3758a8910546782e4f93f9ba07c7541de
Rob Cannon <rcannon@acceleratelearning.com>
2022-06-10 09:14:00 -0500

    Initial commit

---
+++ get-next-container-image-tag.ps1
@@ -0,0 +1,20 @@
+#!/bin/env pwsh
+[CmdletBinding()]
+param (
+    [String]$Registry,
+    [String]$RepositoryName,
+    [String]$MajorMinorVersion
+)
+
+$tag_pattern = "$($MajorMinorVersion.Replace('.','\.'))\.\d+"
+$tags = (aws ecr list-images --repository-name $RepositoryName | ConvertFrom-Json).imageIds.imageTag
+$greatest_tag = $tags | Where-Object { $_ -match $tag_pattern } | ForEach-Object { [Version]"$_" } | Sort-Object -Descending | Select-Object -First 1
+if (-Not $greatest_tag) {
+    $next_tag = [Version]"$($MajorMinorVersion).0"
+}
+else {
+    $next_tag = [Version]::new($greatest_tag.Major, $greatest_tag.Minor, $greatest_tag.Build + 1)
+}
+$next_image_name = "$Regsitry/$($RepositoryName):$next_tag"
+Write-Host "::set-output name=next-tag::$next_tag"
+Write-Host "::set-output name=next-image-name::$next_image_name"
